9da2a5e5b7bc565b5b3463a986d45540375ecfb7
smithsane <infernalrabbit@gmail.com>
2013-07-20 11:56:25 +0300

    Added 16 pads

--- gen/org/smithsane/androidmpc/R.java
+++ gen/org/smithsane/androidmpc/R.java
@@ -24,7 +24,25 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int LinearLayout1=0x7f080000;
+        public static final int action_settings=0x7f080012;
+        public static final int pad1=0x7f080002;
+        public static final int pad10=0x7f08000b;
+        public static final int pad11=0x7f08000c;
+        public static final int pad12=0x7f08000d;
+        public static final int pad13=0x7f08000e;
+        public static final int pad14=0x7f08000f;
+        public static final int pad15=0x7f080010;
+        public static final int pad16=0x7f080011;
+        public static final int pad2=0x7f080003;
+        public static final int pad3=0x7f080004;
+        public static final int pad4=0x7f080005;
+        public static final int pad5=0x7f080006;
+        public static final int pad6=0x7f080007;
+        public static final int pad7=0x7f080008;
+        public static final int pad8=0x7f080009;
+        public static final int pad9=0x7f08000a;
+        public static final int textView1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
