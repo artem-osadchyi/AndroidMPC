2ba32a2523414ae40222ab9543a1bac563f82e17
Insane <insane@insane-cv.(none)>
2013-07-31 18:53:03 +0300

    A little better arch

--- src/org/smithsane/androidmpc/MainActivity.java
+++ src/org/smithsane/androidmpc/MainActivity.java
@@ -1,54 +1,19 @@
 package org.smithsane.androidmpc;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.media.AudioManager;
-import android.media.SoundPool;
-import android.media.SoundPool.OnLoadCompleteListener;
 import android.view.Menu;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.View.OnTouchListener;
-import android.widget.Button;
-import android.widget.TextView;
+import android.app.Activity;
 
-public class MainActivity extends Activity implements OnTouchListener, OnLoadCompleteListener {
-	private SoundPool soundPool = null;
-	
-	private int sidKick;
-	private int sidSnare;
-	private int sidHat;
-	private int sidCrash;
-
+public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		((Button) findViewById(R.id.pad1)).setOnTouchListener(this);
-		((Button) findViewById(R.id.pad2)).setOnTouchListener(this);
-		((Button) findViewById(R.id.pad3)).setOnTouchListener(this);
-		((Button) findViewById(R.id.pad4)).setOnTouchListener(this);
-		((Button) findViewById(R.id.pad5)).setOnTouchListener(this);
-		((Button) findViewById(R.id.pad6)).setOnTouchListener(this);
-		((Button) findViewById(R.id.pad7)).setOnTouchListener(this);
-		((Button) findViewById(R.id.pad8)).setOnTouchListener(this);
-		((Button) findViewById(R.id.pad9)).setOnTouchListener(this);
-		((Button) findViewById(R.id.pad10)).setOnTouchListener(this);
-		((Button) findViewById(R.id.pad11)).setOnTouchListener(this);
-		((Button) findViewById(R.id.pad12)).setOnTouchListener(this);
-		((Button) findViewById(R.id.pad13)).setOnTouchListener(this);
-		((Button) findViewById(R.id.pad14)).setOnTouchListener(this);
-		((Button) findViewById(R.id.pad15)).setOnTouchListener(this);
-		((Button) findViewById(R.id.pad16)).setOnTouchListener(this);
-		
-		soundPool = new SoundPool(4, AudioManager.STREAM_MUSIC, 0);
-		soundPool.setOnLoadCompleteListener(this);
-		
-		sidCrash = soundPool.load(this, R.raw.crash, 1);
-		sidHat = soundPool.load(this, R.raw.hat, 1);
-		sidKick = soundPool.load(this, R.raw.kick, 1);
-		sidSnare = soundPool.load(this, R.raw.snare, 1);
+		Pad pad1 = new Pad(findViewById(R.id.pad1), this, R.raw.kick);
+		Pad pad2 = new Pad(findViewById(R.id.pad2), this, R.raw.snare);
+		Pad pad3 = new Pad(findViewById(R.id.pad3), this, R.raw.crash);
+		Pad pad4 = new Pad(findViewById(R.id.pad4), this, R.raw.hat);
 	}
 
 	@Override
@@ -57,31 +22,4 @@ public class MainActivity extends Activity implements OnTouchListener, OnLoadCom
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
-	@Override
-	public boolean onTouch(View view, MotionEvent event) {
-		((TextView)findViewById(R.id.textView1)).setText(view.getTag().toString());
-		
-		if (event.getAction() == MotionEvent.ACTION_DOWN) {
-			switch (view.getId()) {
-				case R.id.pad1:
-					soundPool.play(sidKick, 1.0f, 1.0f, 1, 0, 1.0f);
-					break;
-				case R.id.pad2:
-					soundPool.play(sidSnare, 1.0f, 1.0f, 1, 0, 1.0f);
-					break;
-				case R.id.pad3:
-					soundPool.play(sidHat, 1.0f, 1.0f, 1, 0, 1.0f);
-					break;
-				case R.id.pad4:
-					soundPool.play(sidCrash, 1.0f, 1.0f, 1, 0, 1.0f);
-					break;
-			}
-		}
-		
-		return false;
-	}
-
-	@Override
-	public void onLoadComplete(SoundPool soundPool, int sid, int status) {}
 }
